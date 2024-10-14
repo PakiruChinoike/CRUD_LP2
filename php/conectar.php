ebc7b4147382aa3f53b701f3ff6983c014dd996a
PakiruChinoike <vhfumagalli@gmail.com>
2024-10-14 08:40:30 -0300

    Conexão com o Banco MySQL

---
+++ php/conectar.php
@@ -0,0 +1,13 @@
+<?php
+    $ip='localhost';
+    $usuario='root';
+    $senha='root';
+    $bd='crud_lp';
+
+    $conexao=mysqli_connect('$ip', '$usuario', '$senha', '$bd');
+
+    // if(!$conexao) {
+    //     die("Status: falha");
+    // } 
+    // echo "Status: sucesso";
+?>
